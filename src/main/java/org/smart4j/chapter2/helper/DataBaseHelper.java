af60ae71c0a6239524d5a37af595b245c44ab0de
YangJing826299017 <826299017@qq.com>
2017-08-05 15:47:50 +0800

    2017年8月5日15:47:46 提交

--- src/main/java/org/smart4j/chapter2/helper/DataBaseHelper.java
+++ src/main/java/org/smart4j/chapter2/helper/DataBaseHelper.java
@@ -3,14 +3,21 @@ package org.smart4j.chapter2.helper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.util.CollectionUtil;
 import org.smart4j.chapter2.util.PropsUtil;
 
 /**
@@ -107,5 +114,136 @@ public class DataBaseHelper {
         
         return entity;
     }
+    
+    //查询语句 返回列与值的映射(Map)
+    public static List<Map<String,Object>> executeQuery(String sql,Object...params){
+        List<Map<String,Object>> result=null;
+        Connection conn=getConnection();
+        try {
+            result=QUERY_RUNNER.query(conn, sql,new MapListHandler(), params);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            LOGGER.error("查询语句发生异常:sql\t"+sql,e);
+            e.printStackTrace();
+        }finally {
+            closeConnection();
+        }
+        return result;
+    }
+    
+    //更新语句(update\insert\delete)
+    public static int executeUpdate(String sql,Object... params){
+        int result=0;
+        Connection conn=getConnection();
+        try {
+            result=QUERY_RUNNER.update(conn, sql, params);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            LOGGER.error("查询语句发生异常:sql\t"+sql,e);
+            e.printStackTrace();
+        }finally {
+            closeConnection();
+        }
+        return result;
+    }
+    
+    //插入实体
+    /**
+     * 插入数据
+     * @param classEntity 实体类
+     * @param filedMap 列名与值的映射列表
+     * @return
+     */
+    public static boolean insertEntity(Class<?> classEntity,Map<String,Object> filedMap){
+        //1.判断进入fileMap 映射是否为空
+        boolean isFiledMapNull=CollectionUtil.isEmpty(filedMap);
+        if(isFiledMapNull){
+            LOGGER.error("列名与值的映射表为空");
+            return false;
+        }
+        //2.拼接Sql
+        //insert into table (id,name) values (?,?)
+        String sql="insert into "+getTableName(classEntity);
+        StringBuilder colums=new StringBuilder("(");
+        StringBuilder values=new StringBuilder("(");
+        int filedMapSize=filedMap.size();
+        Set<String> keySet=filedMap.keySet();
+        Iterator<String> it=keySet.iterator();
+        int count=1;
+        while(it.hasNext()){
+              String oneColum=it.next();
+              boolean isLastElement=filedMapSize==count;
+              if(isLastElement){
+                  colums.append(oneColum+")");
+                  values.append("?)");
+              }
+              if(!isLastElement){
+                  colums.append(oneColum+",");
+                  values.append("?,");
+              }
+              count++;
+        }
+        String finalSql=sql+ colums.toString()+ " values "+values.toString();
+        //3.插入
+        Object[] params=filedMap.values().toArray();
+        int result=executeUpdate(finalSql, params);
+        return result==1;
+    }
+    
+    //删除实体
+    public static boolean deleteEntity(Class<?> classEntity,Long id){
+        String tableName=getTableName(classEntity);
+        StringBuilder sb=new StringBuilder("delete from "+tableName+" where id="+id);
+        int result=executeUpdate(sb.toString(),id);
+        return result==1;
+    }
+    
+    //更新实体
+    public static boolean updateEntity(Class<?> classEntity,Map<String,Object> filedMap,long id){
+        //1.判断进入fileMap 映射是否为空
+        boolean isFiledMapNull=CollectionUtil.isEmpty(filedMap);
+        if(isFiledMapNull){
+            LOGGER.error("列名与值的映射表为空");
+            return false;
+        }
+        //2.拼接Sql
+        //update customer set name=?,sex=? where id=?
+        String sql="update "+getTableName(classEntity)+" set ";
+        StringBuilder columsAndValue=new StringBuilder(" ");
+        int filedMapSize=filedMap.size();
+        Set<String> keySet=filedMap.keySet();
+        Iterator<String> it=keySet.iterator();
+        int count=1;
+        while(it.hasNext()){
+              String oneColum=it.next();
+              boolean isLastElement=filedMapSize==count;
+              if(isLastElement){
+                  columsAndValue.append(oneColum+"=? ");
+              }
+              if(!isLastElement){
+                  columsAndValue.append(oneColum+"=?,");
+              }
+              count++;
+        }
+        StringBuilder whereSql=new StringBuilder(" where id="+id);
+        String finalSql=sql+columsAndValue.toString()+whereSql.toString();
+        //3.更新
+        Object[] params=filedMap.values().toArray();
+        int result=executeUpdate(finalSql, params);
+        return result==1;
+    }
+    
+    public static void main(String[] args) {
+        Map<String,Object> map=new HashMap<>();
+        map.put("name","杨景");
+        map.put("sex","男");
+        DataBaseHelper.updateEntity(Customer.class, map, 15240069l);
+    }
+    
+    
+    private static String getTableName(Class<?> classEntity){
+        return classEntity.getSimpleName();
+    }
+    
 
 }
--- src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,6 +1,7 @@
 package org.smart4j.chapter2.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.smart4j.chapter2.helper.DataBaseHelper;
 import org.smart4j.chapter2.model.Customer;
@@ -15,6 +16,23 @@ public class CustomerService {
     public Customer getCustomer(Integer id){
         return DataBaseHelper.getEntity(Customer.class, "select * from customer where id=?",id);
     }
+    
+    //创建客户
+    public boolean createCustomer(Map<String,Object> filedMap){
+        return DataBaseHelper.insertEntity(Customer.class, filedMap);
+    }
+    
+    //删除客户
+    public boolean deleteCustomer(long id){
+        return DataBaseHelper.deleteEntity(Customer.class, id);
+    }
+    
+    //更新用户
+    public boolean updateCustomer(Map<String,Object> filedMap,long id){
+        return DataBaseHelper.updateEntity(Customer.class, filedMap, id);
+    }
+    
+    
     
     
 
